314633efbb67fe149308ded076a6e2e7b0a1f900
Ridwanah Ali <ridwanah.ali221@gmail.com>
2025-03-28 17:10:34 +0000

    feat: implement PostcodeView for restaurant data API integration.

--- justeat/restaurant/views.py
+++ justeat/restaurant/views.py
@@ -1,6 +1,28 @@
 from django.shortcuts import render
-from rest_framework import status
-from rest_framework.decorators import api_view
+from rest_framework.views import APIView
 from rest_framework.response import Response
+import requests
 
-# Create your views here.
+def index(request):
+    return render(request, 'restaurant/index.html')
+
+class PostcodeView(APIView):
+    def get(self, request, postcode):
+        cleaned_postcode = postcode.replace(' ', '').upper()
+        api_url = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{cleaned_postcode}"
+        headers = {
+            'User-Agent': 'restaurant' 
+        }           
+        response = requests.get(api_url, headers=headers, timeout=5)
+        data = response.json().get('restaurants', [])[:10]
+        restaurant_data = []
+        for restaurant in data:
+            values = {
+                'name' : restaurant.get('name'),
+                'cuisines' : restaurant.get('cuisines'),
+                'rating' : (restaurant.get('rating')).get('starRating'),
+                'address' : restaurant.get('address'),
+            }
+            restaurant_data.append(values)
+        return Response(restaurant_data)
+            
