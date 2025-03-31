8db81050323088ea422093155bb67fe89410e712
Ridwanah Ali <ridwanah.ali221@gmail.com>
2025-03-31 17:14:58 +0100

    chore: add comments in serializers.py, views.py and index.html.

--- justeat/restaurant/serializers.py
+++ justeat/restaurant/serializers.py
@@ -1,8 +1,11 @@
 from rest_framework import serializers
 
+# Serializes cuisine data from Just Eat API response.
 class CuisineSerializer(serializers.Serializer):
     name = serializers.CharField()
 
+# Converts Just Eat API restauarnt data into Python objects
+# based on selected fields" 'name', 'cuisines', 'rating', 'address'.
 class RestaurantSerializer(serializers.Serializer):
     name = serializers.CharField()
     cuisines = CuisineSerializer(many=True)
--- justeat/restaurant/views.py
+++ justeat/restaurant/views.py
@@ -7,15 +7,16 @@ from .serializers import RestaurantSerializer
 def index(request):
     return render(request, 'restaurant/index.html')
 
+# API endpoint for getting restaurant data from Just Eat API.
 class PostcodeView(APIView):
     def get(self, request, postcode):
-        cleaned_postcode = postcode.replace(' ', '').upper()
+        cleaned_postcode = postcode.replace(' ', '').upper() #remove whitespace postcode.
         api_url = f"https://uk.api.just-eat.io/discovery/uk/restaurants/enriched/bypostcode/{cleaned_postcode}"
         headers = {
             'User-Agent': 'restaurant',
         }           
         response = requests.get(api_url, headers=headers, timeout=5)
-        data = response.json().get('restaurants', [])[:10]
+        data = response.json().get('restaurants', [])[:10] #get first 10 results.
         serializer = RestaurantSerializer(data=data, many=True)
         serializer.is_valid(raise_exception=True)
 
